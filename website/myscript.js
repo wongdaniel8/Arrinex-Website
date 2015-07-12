a8bde4ded01b666b5b74939bd59aba25d290b559
Daniel Wong <danielwong@Daniels-MacBook-Pro-4.local>
2015-07-11 19:48:47 -0700

    first commit setup dan

---
+++ website/myscript.js
@@ -0,0 +1,11 @@
+function navBarHover(ident) {
+	var id = document.getElementById(ident);
+	id.style.color = "#606060";
+}
+
+
+function navBarHoverAway(ident) {
+	var id = document.getElementById(ident);
+	id.style.color = "#B8B8B8";
+}
+
