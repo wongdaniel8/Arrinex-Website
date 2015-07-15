2adbca9babe0518cff48351861d6050b88e468bd
Daniel Wong <danielwong@Daniels-MacBook-Pro-4.local>
2015-07-15 15:13:53 -0700

    dan: began adding a hidden description banner for nav bar

--- website/myscript.js
+++ website/myscript.js
@@ -4,8 +4,15 @@
 // }
 
 
-// function navBarHoverAway(ident) {
-// 	var id = document.getElementById(ident);
-// 	id.style.color = "#B8B8B8";
-// }
+function navBarHoverAway(ident, ident2) {
+	var id = document.getElementById(ident);
+	id.style.color = "#B8B8B8";
+    var id2 = document.getElementById(ident2);
+    id2.style.display = "none";
 
+}
+
+function displayHidden(ident) {
+	var id = document.getElementById(ident);
+	id.style.display = "block";
+}
