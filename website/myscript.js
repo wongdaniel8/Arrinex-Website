0c6eff4a8a1abe2e2de038adb6ec53542840e8ae
hbonilla2 <hbonilla212@gmail.com>
2015-07-26 21:19:56 -0700

    herb's baller commit 7/26 #1

--- website/myscript.js
+++ website/myscript.js
@@ -212,44 +212,44 @@ $(document).ready(function(){
 
 
 //////////Display Bio
-    function fullBio(event){
-    	if ($(event + 'Para').css("display") == 'none'){
-    		//$("#sab").css("height", "670px");
-    		//$(event).parent().css("height", "235px");
-    		$("#hwangPara").css("display", "none");
-    		$("#hwangButton").html("&nbsp; &nbsp;Full Bio &darr;");
-    		$("#hwangHeader").css("background", "rgba(255,255,255,.7");
-    		$("#weissPara").css("display", "none");
-    		$("#weissButton").html("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Full Bio &darr;");
-    		$("#weissHeader").css("background", "rgba(255,255,255,.7");
-    		$("#bolgerPara").css("display", "none");
-    		$("#bolgerButton").html("Full Bio &darr;");
-    		$("#bolgerHeader").css("background", "rgba(255,255,255,.7");
-    		$("#goodePara").css("display", "none");
-    		$("#goodeButton").html("&nbsp;&nbsp;&nbsp;&nbsp;Full Bio &darr;");
-    		$("#goodeHeader").css("background", "rgba(255,255,255,.7");
-    		$("#nelsonPara").css("display", "none");
-    		$("#nelsonButton").html("Full Bio &darr;");
-    		$("#nelsonHeader").css("background", "rgba(255,255,255,.7");
-    		$("#stromePara").css("display", "none");
-    		$("#stromeButton").html("Full Bio &darr;");
-    		$("#stromeHeader").css("background", "rgba(255,255,255,.7");
-    		$(event + 'Para').css("transition", "display .7s ease");
-    		$(event + 'Para').css("display", "inline");
-    		$(event + 'Button').html("Full Bio &uarr;");
-    		$(event + 'Header').css("transition", "background .7s ease");
-    		$(event + 'Header').css("background", "rgba(125,125,125,.7");
+    // function fullBio(event){
+    // 	if ($(event + 'Para').css("display") == 'none'){
+    // 		//$("#sab").css("height", "670px");
+    // 		//$(event).parent().css("height", "235px");
+    // 		$("#hwangPara").css("display", "none");
+    // 		$("#hwangButton").html("&nbsp; &nbsp;Full Bio &darr;");
+    // 		$("#hwangHeader").css("background", "rgba(255,255,255,.7");
+    // 		$("#weissPara").css("display", "none");
+    // 		$("#weissButton").html("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Full Bio &darr;");
+    // 		$("#weissHeader").css("background", "rgba(255,255,255,.7");
+    // 		$("#bolgerPara").css("display", "none");
+    // 		$("#bolgerButton").html("Full Bio &darr;");
+    // 		$("#bolgerHeader").css("background", "rgba(255,255,255,.7");
+    // 		$("#goodePara").css("display", "none");
+    // 		$("#goodeButton").html("&nbsp;&nbsp;&nbsp;&nbsp;Full Bio &darr;");
+    // 		$("#goodeHeader").css("background", "rgba(255,255,255,.7");
+    // 		$("#nelsonPara").css("display", "none");
+    // 		$("#nelsonButton").html("Full Bio &darr;");
+    // 		$("#nelsonHeader").css("background", "rgba(255,255,255,.7");
+    // 		$("#stromePara").css("display", "none");
+    // 		$("#stromeButton").html("Full Bio &darr;");
+    // 		$("#stromeHeader").css("background", "rgba(255,255,255,.7");
+    // 		$(event + 'Para').css("transition", "display .7s ease");
+    // 		$(event + 'Para').css("display", "inline");
+    // 		$(event + 'Button').html("Full Bio &uarr;");
+    // 		$(event + 'Header').css("transition", "background .7s ease");
+    // 		$(event + 'Header').css("background", "rgba(125,125,125,.7");
 
-    	} else{
-    		//$(event).parent().css("height", "100px");
-    		//$("#sab").css("height", "270px");
-    		$(event + 'Header').css("transition", "background .7s ease");
-    		$(event + 'Header').css("background", "rgba(255,255,255,.7");
-    		$(event + 'Para').css("transition", "display .7s ease");
-    		$(event + 'Para').css("display", "none");
-    		$(event + 'Button').html("Full Bio &darr;");
-    	}
-    }
+    // 	} else{
+    // 		//$(event).parent().css("height", "100px");
+    // 		//$("#sab").css("height", "270px");
+    // 		$(event + 'Header').css("transition", "background .7s ease");
+    // 		$(event + 'Header').css("background", "rgba(255,255,255,.7");
+    // 		$(event + 'Para').css("transition", "display .7s ease");
+    // 		$(event + 'Para').css("display", "none");
+    // 		$(event + 'Button').html("Full Bio &darr;");
+    // 	}
+    // }
 		
 
 /////////////////PASSWORD SCRIPT START///////////////////////
@@ -492,7 +492,114 @@ function playBoyGearMotion()
 
 
 
+$(document).on('scroll', function(e) {
+	
+        var S = $(this).scrollTop() - 3630,                // scrolled distance
+            T = 315 + (S/45),                        // value for Top
+            L = 3 + (S/24)							//value for Left
+            H = 100*(S/75)							//value for height
+            W = 100*(S/70)							//value for width
+            D = 360 *(S/100)						//value for degrees
+            //L = 10 + Math.abs(Math.sin(S/40)*50);  // value for Left
+            //set CSS
+        if(H > 10 && H <= 401){
+        	$("#dandelion").css({"margin-top": T + 'px'});
+        	$("#dandelion").css({"margin-left": L + '%'});
+        	$("#dandelion").css({"height": H + 'px'});
+        	$("#dandelion").css({"width": W + 'px'});
+        	$("#dandelion").css({"transform": 'rotate(' + D + 'deg)'});
+            $("#sabHeader").css({"transition": "display 1s"});
+            $("#sabHeader").css({"display": "none"});
+            if(H<=390){
+            $("#hwangHeader").css({"transition": "display 1s"});
+            $("#hwangHeader").css({"display": "none"});
+            $("#weissHeader").css({"transition": "display 1s"});
+            $("#weissHeader").css({"display": "none"});
+            $("#stromeHeader").css({"transition": "display 1s"});
+            $("#stromeHeader").css({"display": "none"});
+            $("#bolgerHeader").css({"transition": "display 1s"});
+            $("#bolgerHeader").css({"display": "none"});
+            $("#goodeHeader").css({"transition": "display 1s"});
+            $("#goodeHeader").css({"display": "none"});
+            $("#nelsonHeader").css({"transition": "display 1s"});
+            $("#nelsonHeader").css({"display": "none"});
+            $("#bud").css({"transition": "display .1s"});
+            $("#bud").fadeOut();
+            $("#bud1").css({"transition": "display 1s"});
+            $("#bud1").fadeOut();
+            $("#bud2").css({"transition": "display 1s"});
+            $("#bud2").fadeOut();
+            $("#bud3").css({"transition": "display 1s"});
+            $("#bud3").fadeOut();
+            $("#bud4").css({"transition": "display 1s"});
+            $("#bud4").fadeOut();
+            $("#bud5").css({"transition": "display 1s"});
+            $("#bud5").fadeOut();
+            $("#hwangPara").css("display", "none");
+            $("#weissPara").css("display", "none");
+            $("#stromePara").css("display", "none");
+            $("#bolgerPara").css("display", "none");
+            $("#goodePara").css("display", "none");
+            $("#nelsonPara").css("display", "none");
+        }
+        }
+        if(H >= 390){
+        	$("#sabHeader").css({"transition": "display .5s"});
+        	$("#sabHeader").css({"display": "block"});
+        	$("#hwangHeader").css({"transition": "display .5s"});
+        	$("#hwangHeader").fadeIn();
+            $("#hwangHeader").css({"display": "inline-block"});
+        	$("#weissHeader").css({"transition": "display 1s"});
+        	$("#weissHeader").fadeIn();
+            $("#weissHeader").css({"display": "inline-block"});
+        	$("#stromeHeader").css({"transition": "display 1.5s"});
+        	$("#stromeHeader").fadeIn();
+        	$("#bolgerHeader").css({"transition": "display 2s"});
+        	$("#bolgerHeader").fadeIn();
+        	$("#goodeHeader").css({"transition": "display 2.5s"});
+        	$("#goodeHeader").fadeIn();
+        	$("#nelsonHeader").css({"transition": "display 3s"});
+        	$("#nelsonHeader").fadeIn();
+            $("#bud").css({"transition": "display .5s"});
+        	$("#bud").fadeIn();
+        	$("#bud1").css({"transition": "display 1s"});
+            $("#bud1").fadeIn();
+        	$("#bud2").css({"transition": "display 1.5s"});
+            $("#bud2").fadeIn();
+        	$("#bud3").css({"transition": "display 2s"});
+            $("#bud3").fadeIn();
+        	$("#bud4").css({"transition": "display 10s ease 3s"});
+            $("#bud4").fadeIn();
+        	$("#bud5").css({"transition": "display 15s linear 5s"});
+            $("#bud5").fadeIn();
+        }
+    });
 
+function sabClick(event){
+	$("#hwangPara").css("display", "none");
+    $("#weissPara").css("display", "none");
+   	$("#stromePara").css("display", "none");
+    $("#bolgerPara").css("display", "none");
+    $("#goodePara").css("display", "none");
+    $("#nelsonPara").css("display", "none");
+	$(event + 'Para').css("text-align", "center");
+    $(event + 'Para').css("width", "300px");
+    $(event + 'Para').css("height", "200px");
+    $(event + 'Para').css("transition", "display .7s");
+    $(event + 'Para').css("display", "inline-block");
+}
+
+var angle = 0;
+setInterval(function(){
+      angle += .33;
+      $("#bud").css({"transform": 'rotate(' + angle + 'deg)'});
+      $("#bud1").css({"transform": 'rotate(' + angle + 'deg)'});
+      $("#bud2").css({"transform": 'rotate(' + angle + 'deg)'});
+      $("#bud3").css({"transform": 'rotate(' + angle + 'deg)'});
+      $("#bud4").css({"transform": 'rotate(' + angle + 'deg)'});
+      $("#bud5").css({"transform": 'rotate(' + angle + 'deg)'});
+     //$("#bud").rotate(angle);
+}, 50);
 	//============================================================
 	// JAVASCRIPT FOR INVESTORS PAGE
 	//============================================================
