5d464d521241d513bd8cab365cb6697803f84901
Devin <devinm.mit.edu>
2015-08-10 02:44:59 -0400

    Devin: fixed last major bugs in investors page and commented code

--- website/investorsScript.js
+++ website/investorsScript.js
@@ -2,9 +2,8 @@
 // JAVASCRIPT FOR INVESTORS PAGE
 //============================================================
     
-// firefox and safari lag hard when the PDFs are directly embedded into the cite so we
-
-
+// firefox and safari lag hard when the PDFs are directly embedded into the cite and loaded all at once
+// therefore each one of these the pdf objects is stored as a string here so it can be inserted at the approriate time not all at once
 var executive_pdf =  '<object data="executive.pdf" type="application/pdf" width="100%" height="100%"> </object>';
 var AR_and_its_impact_on_productivity = '<object data="AR & its impact on work productivity.pdf" type="application/pdf" width="100%" height="100%"> </object>';
 var canning_neurology_of_AR = '<object data="Canning_ neurology of Allergic inflammation and rhinitis.pdf" type="application/pdf" width="100%" height="100%"> </object>';
@@ -16,21 +15,16 @@ var prevelence = '<object data="prevalence.pdf" type="application/pdf" width="10
 var businitch = '<object data="businitch.pdf" type="application/pdf" width="100%" height="100%"> </object>';
 
 
+// the drop down is toggled by a hover and a mouseleave mechanism so it tends to call the showDropDown/ hideDropDown function too many times and it lags
 
-$(document).ready(function(){
-	// FUNCTION to reveal document navigator
-		
-});
-
-var dd_animation_in_progress = false;
+var dd_animation_in_progress = false; // <---- this variable keeps track of whether an animation is already in progress to avoid it getting called again if it is already running
 
 function showDropDown()
 {
 	if (dd_animation_in_progress == false)
 	{
-		window.console.log((dd_animation_in_progress == false)+ "   show");
 		$("#documentDDContainer").css("display","block");
-		$("#documentDDContainer").parent().html($("#documentDDContainer").parent().html());
+		$("#documentDDContainer").parent().html($("#documentDDContainer").parent().html()); //this replaces the innerHTML with itself because CSS animations only work once so they HTML must be refreshed for it to keep working
 		$("#documentDDContainer").removeClass("ddUp").addClass("ddDown");
 		
 		$("#docDescriptionList, #descListHeader").css("display","block");
@@ -45,15 +39,11 @@ function hideDropDown()
 {
 	if (dd_animation_in_progress == true)
 	{
-		window.console.log((dd_animation_in_progress == true) + "   hide");
 		$("#documentDDContainer").parent().html($("#documentDDContainer").parent().html());
 		$("#documentDDContainer").removeClass("ddDown").addClass("ddUp");
 		$("#docDescriptionList, #descListHeader").css("display","none");
 		
-
-			dd_animation_in_progress = false;
-
-
+		dd_animation_in_progress = false;
 	}
 	
 }
@@ -63,8 +53,8 @@ function hideDropDown()
 function selectMiniArticle(article_title,item)
 {
 	// get the HTMl for the PDF object file 
-	var mini_article_HTML = '<div class = "smallPDF">' + window[article_title] +'</div>';
-							// alert(window[article_title]);
+	var mini_article_HTML = '<div class = "smallPDF">' + window[article_title] +'</div>'; // <div class = "smallPDF"> and </div> wrap around the mini articles to dictate the size of the PDF
+
 	//hide instructions
 	$("#instructions").css("display","none");
 
@@ -77,16 +67,15 @@ function selectMiniArticle(article_title,item)
 	$(".previewItem[data-art-num="+article_title+"]").html(mini_article_HTML);
 
 	// underline title of slected document
-	
 	$(".documentListLI").css("border-bottom","1px solid transparent");
 	$(item).parent().css("border-bottom","1px solid black");
-	// this.style.borderBottom = "2px solid black";
+
 }
 
 function selectMainArticleAndHideDropDown(article_title,item)
 {
 	//reset the drop down menu
-		// hide all previews
+		// hide all previewItems (aka all the miniPDFs)
 		$(".previewItem").css("display","none");
 
 		//erase the underline
@@ -103,45 +92,43 @@ function selectMainArticleAndHideDropDown(article_title,item)
 
 }
 
-var currentArticleTitle = "executive_pdf";
-var permit_retraction = false;
+var currentArticleTitle = "executive_pdf"; // <--- the page starts off display the Executive Summary PDF and this variable holds the currently visible article
+var permit_retraction = false; // this variable indicates whether or not the vacuum can begin retracting (away to indicate that it has finished its job and can disappear)
 
 function deployVacuum(article_title,item)
 {
 	permit_retraction = true;
+	$( "#vert_extend_rect" ).css("display","block");
 	$( "#vert_extend_rect" ).animate({
 
-							height: "+=350"
+							height: "420px" //extends the vertical rectangle down (increasing its height)
 
-	 	}, 300, function() {
+	 	}, 200, function() {
 		    // vert_extend_rect COMPLETE.
 		    $("#horz_extend_rect").css("display","block");
 		    $("#horz_extend_rect").animate({
 
-		          					width: "+=100"
+		          					width: "150px" // extends the horizontal rectangle to the right (increaseing its width)
 
-			    }, 300, function() {
+			    }, 200, function() {
 			    	// horz_extend_rect COMPLETE.
 			    	$("#vacuum_nosel").css("display","block");
 			    	$("#vacuum_nosel").animate({
 
-			          					height: "+=50",
-			          					top: "-=25",
-			          					borderTop: "120px solid transparent", 
-									    borderRight: "100px solid blue", 
-									    borderBottom: "120px solid transparent",
+			          					height: "+=50",								// this animates the vacuum nosel: height of the nosel is determined by
+			          					top: "345px",
 									    right: "-30px"
 
-				    	}, 300, function() { 
+				    	}, 200, function() { 
 				    		// vacuum_nosel COMPLETE.
 				    		$(".article[data-art-num="+currentArticleTitle+"]").find(".trapazoidTop").addClass("trapazoidTopLive");
 				    		$(".article[data-art-num="+currentArticleTitle+"]").find(".trapazoidBot").addClass("trapazoidBotLive");
-				    		var time_to_slid = window.innerWidth * -0.5 + 1300;
+				    		var time_to_slide = window.innerWidth * -0.5 + 1300;  //<--- this is equation gives the time article should take moving the the left as a function of window.innerWidth
 				    		$(".article[data-art-num="+currentArticleTitle+"]").animate({
 
 				    			right: "1300px"
 
-				    			}, time_to_slid, function(){ //1600 --> 500 1400 --> 600  1200 --> 700     600 = -1/2x + 1300
+				    			}, time_to_slide, function(){ 
 
 				    				// article COMPLETE
 				    					
@@ -151,11 +138,11 @@ function deployVacuum(article_title,item)
 				    					$(".article[data-art-num="+currentArticleTitle+"]").find(".trapazoidBot").removeClass("trapazoidBotLive");
 
 				    					
-				    					var main_article_HTML = window[article_title] + '<div class="trapazoidTop"></div>' + '<div class="trapazoidBot"></div>';
+				    					var main_article_HTML = window[article_title] + '<div class="trapazoidTop"></div>' + '<div class="trapazoidBot"></div>'; //<--- the suction animation is actually 2 right triangles that grow larger and cut off the ducument which is the purpose of the '<div class="trapazoidTop"></div>' + '<div class="trapazoidBot"></div>'
 
 										//insert the HTML after clearing all others
 										$(".article").html("");
-										$(".article[data-art-num="+article_title+"]").html(main_article_HTML);
+										$(".article[data-art-num="+article_title+"]").html(main_article_HTML); 
 
 
 				    					// get rid of the vacuum device
@@ -184,36 +171,34 @@ function deployVacuum(article_title,item)
 
 function retractVacuum()
 {
-	if (permit_retraction == true)
+	if (permit_retraction == true) // only retract the vacuum when the document has left the screen
 	{
-		permit_retraction = false;
+		permit_retraction = false; // once it starts to retract it should not call the retractVacuum() function again until it is done retracting
 		// retract blue triangle
-		$("#vacuum_nosel").animate({
+		$("#vacuum_nosel").animate({      //the following animations are just the reverse of the animations that extend the vacuum device contraption
 
 							height: "-=50",
-	      					top: "+=25",
-	      					borderTop: "25px solid transparent", 
-						    borderRight: "50px solid blue", 
-						    borderBottom: "25px solid transparent",
+	      					top: "370px",
 						    right: "0px"
 
 
-			}, 300, function(){
+			}, 200, function(){
 				// nosel COMPLETE
 				$("#vacuum_nosel").css("display","none");
 				$("#horz_extend_rect").animate({
 
-										width: "-=100"
+										width: "50px"
 
-					}, 300, function() {
+					}, 200, function() {
 						// horz_extend_rect COMPLETE
 						$("#horz_extend_rect").css("display","none");
 						$("#vert_extend_rect").animate({
 
-												height: "-=350"
+												height: "70px"
 
-							}, 300, function(){
+							}, 200, function(){
 								//vert_extend_rect COMPLETE
+								$( "#vert_extend_rect" ).css("display","none");
 
 						});
 
